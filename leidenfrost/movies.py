c099c2130f238c4eb8c0d7128a0acd3178cf7cd8
Thomas A Caswell <tcaswell@uchicago.edu>
2013-01-18 20:51:33 -0600

    mostly pep8 fixes

--- leidenfrost/movies.py
+++ leidenfrost/movies.py
@@ -19,30 +19,30 @@ from __future__ import division
 
 import fractions
 
-import h5py
-import cine
+
 import numpy as np
 
-import matplotlib
+
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.animation as animation
 
-import leidenfrost.db as db
+
 import leidenfrost.infra as infra
 
 
 class AnimateHdf(object):
     '''A class to wrap up all the infrastructure
     needed to make nice movies out of processed hdf files '''
-    def __init__(self,hdf_backend,max_frame=None,min_frame=0,step=1):
+    def __init__(self, hdf_backend, max_frame=None, min_frame=0, step=1):
 
         # backend
         self.backend = hdf_backend
         self._update_funs = []
 
         # animation limits
-        if max_frame is None or max_frame > len(self.backend) or max_frame < min_frame:
+        if max_frame is None or (max_frame > len(self.backend) or
+                                 max_frame < min_frame):
             max_frame = len(self.backend)
 
         self.max_frame = max_frame
@@ -50,7 +50,6 @@ class AnimateHdf(object):
         self.step = step
 
         limits = self._prep_img_limits()
-
 
         # figure setup
         fig = plt.figure()
@@ -60,7 +59,6 @@ class AnimateHdf(object):
         mi = MarkedupImage(img_ax,
                            limits,
                            draw_fringes=False)
-
 
         zprof_ax = fig.add_subplot(gs[0, 1])
         zp = ZProfile(zprof_ax)
@@ -72,23 +70,24 @@ class AnimateHdf(object):
         self._update_funs.append(zp.update)
         self._update_funs.append(rp.update)
 
-        fig.text(0, 1, '/'.join(self.backend.cine_fname[1:]).replace('_','\_'), va='top')
-        self.frame_label = fig.text(0,0,'')
+        fig.text(0, 1,
+                 '/'.join(self.backend.cine_fname[1:]).replace('_', '\_'),
+                 va='top')
+        self.frame_label = fig.text(0, 0, '')
 
         gs.tight_layout(fig)
 
-        fig.set_size_inches(9.6,5.4,forward=True)
+        fig.set_size_inches(9.6, 5.4, forward=True)
         # do the animation
-        self.ani =  animation.FuncAnimation(fig,
-                                            self._update_figure,
-                                            range(min_frame,max_frame,step),
-                                            interval=25)
+        self.ani = animation.FuncAnimation(fig,
+                                           self._update_figure,
+                                           range(min_frame, max_frame - 1, step),
+                                           interval=25)
 
-    def save(self,fpath):
+    def save(self, fpath):
 
-        writer = animation.writers['ffmpeg'](fps=20,bitrate=30000)
-        self.ani.save(fpath,writer=writer,dpi=200)
-
+        writer = animation.writers['ffmpeg'](fps=20, bitrate=30000)
+        self.ani.save(fpath, writer=writer, dpi=200)
 
     def _prep_img_limits(self):
         '''Figures out what the extent of the
@@ -98,10 +97,10 @@ class AnimateHdf(object):
         max_y = -np.inf
         min_x = np.inf
         min_y = np.inf
-        imgY,imgX = self.backend.get_frame(0,raw=False,img=True).img.shape
-        for j in range(self.min_frame,self.max_frame,frame_step):
-            x,y = self.backend.get_frame(j, False, False).curve.q_phi_to_xy(0, np.linspace(0, 2 * np.pi, 1000))
-
+        imgY, imgX = self.backend.get_frame(0, raw=False, img=True).img.shape
+        for j in range(self.min_frame, self.max_frame, frame_step):
+            mbe = self.backend.get_frame(j, False, False)
+            x, y = mbe.curve.q_phi_to_xy(0, np.linspace(0, 2 * np.pi, 1000))
 
             if np.max(x) > max_x:
                 max_x = np.max(x)
@@ -124,13 +123,14 @@ class AnimateHdf(object):
 
         return [min_x, max_x, min_y, max_y]
 
-    def _update_figure(self,j):
+    def _update_figure(self, j):
         '''Updates the figure, wraps up all of the axes '''
         res = ()
         for f in self._update_funs:
             res = res + f(self.backend, j)
         self.frame_label.set_text('frame %d' % j)
         return res + (self.frame_label, )
+
 
 def format_frac(fr):
     sp = str(fr).split('/')
@@ -139,37 +139,39 @@ def format_frac(fr):
     else:
         return r'$\frac{%s}{%s}$' % tuple(sp)
 
+
 class ZProfile(object):
     def __init__(self, ax):
         self.ax = ax
-        self.prof_ln, = self.ax.plot([], [],'.k')
-        self.ax.set_xlim([0,2 * np.pi])
+        self.prof_ln, = self.ax.plot([], [], '.k')
+        self.ax.set_xlim([0, 2 * np.pi])
         self.ax.set_ylim([0, 20])
         ax.set_xlabel(r'$\theta$')
         ax.set_ylabel('$\Delta h$ [um]')
         frac_size = 4
         step = fractions.Fraction(1, frac_size)
         ax.set_xticks([np.pi * j * step for j in range(2 * frac_size + 1)])
-        ax.set_xticklabels([format_frac(j * step) + '$\pi$' for j in range(2 * frac_size + 1)])
+        ax.set_xticklabels([format_frac(j * step) + '$\pi$'
+                            for j in range(2 * frac_size + 1)])
 
         ax.axvline(.5 * np.pi, color='k', linestyle='--')
         ax.axvline(np.pi, color='k', linestyle='--')
         ax.axvline(1.5 * np.pi, color='k', linestyle='--')
 
-    def update(self, backend, j, delta_h = 0):
+    def update(self, backend, j, delta_h=0):
         mbe = backend.get_frame(j, False, False)
 
         th = np.hstack([mbe.res[0][1], mbe.res[1][1]])
         ch = np.hstack([mbe.res[0][0], mbe.res[1][0]])
 
-        XY = np.vstack(mbe.curve.q_phi_to_xy(0,np.linspace(0,2 * np.pi,2**10)))
+        XY = np.vstack(mbe.curve.q_phi_to_xy(0,
+                                            np.linspace(0, 2 * np.pi, 2 ** 10)))
         center = np.mean(XY, 1)
-        first_pt = center - XY[:,0]
+        first_pt = center - XY[:, 0]
 
-        th_offset = np.arctan2(first_pt[1],first_pt[0])
+        th_offset = np.arctan2(first_pt[1], first_pt[0])
 
-
-        dh,th_new = infra.construct_corrected_profile((th,ch),th_offset)
+        dh, th_new = infra.construct_corrected_profile((th, ch), th_offset)
         dh -= np.min(dh)
         dh += delta_h
         dh *= (.543 / 4)
@@ -178,23 +180,22 @@ class ZProfile(object):
         self.prof_ln.set_ydata(dh)
         return (self.prof_ln, )
 
+
 class RProfile(object):
-    def __init__(self,ax):
+    def __init__(self, ax):
         self.ax = ax
         ax.cla()
-        ax.set_xlim([0,2 * np.pi])
+        ax.set_xlim([0, 2 * np.pi])
         ax.set_ylim(-50, 50)
-        self.th = np.linspace(0, 2 * np.pi,1000)
-        th = self.th
-        self.line, = ax.plot(th, np.zeros(th.shape), 'b-')
-
+        th = np.linspace(0, 2 * np.pi, 1000)
+        self.th = th
+        self.line, = ax.plot([], [], 'b-')
 
         ax.set_xlabel(r'$\theta$')
         ax.set_ylabel(r'$R - \langle R \rangle$ [pix]')
-        step = fractions.Fraction(1,4)
+        step = fractions.Fraction(1, 4)
         ax.set_xticks([np.pi * j * step for j in range(9)])
         ax.set_xticklabels([format_frac(j * step) + '$\pi$' for j in range(9)])
-
 
         ax.axhline(0, color='k', linestyle='--')
 
@@ -202,16 +203,16 @@ class RProfile(object):
         ax.axvline(np.pi, color='k', linestyle='--')
         ax.axvline(1.5 * np.pi, color='k', linestyle='--')
 
-
     def update(self, backend, j):
         mbe = backend.get_frame(j + 1, False, False)
-        q,phi = mbe.res[0][2], mbe.res[0][1]
-        new_curve = infra.SplineCurve.from_pts(np.vstack(mbe.curve.q_phi_to_xy(q, phi)),
+        q, phi = mbe.res[0][2], mbe.res[0][1]
+        new_curve = infra.SplineCurve.from_pts(np.vstack(mbe.curve.q_phi_to_xy(q,
+                                                                               phi)),
                                                **mbe.params)
         new_curve.fft_filter(mbe.params['fft_filter'])
-        XY  = np.vstack(new_curve.q_phi_to_xy(0,self.th))
+        XY = np.vstack(new_curve.q_phi_to_xy(0, self.th))
 
-        center = np.mean(XY, 1).reshape(2,1)
+        center = np.mean(XY, 1).reshape(2, 1)
 
         XY -= center
 
@@ -219,30 +220,23 @@ class RProfile(object):
 
         R -= np.mean(R)
 
-        th = np.arctan2(-XY[1,:],-XY[0,:])
-
-
-
-
-        th_offset = np.arctan2(-XY[1,0],-XY[0,0])
+        th_offset = np.arctan2(-XY[1, 0], -XY[0, 0])
 
         self.line.set_ydata(R)
         x_data = np.mod(self.th + th_offset, 2 * np.pi)
         abs_d_data = np.abs(np.diff(x_data))
-        mask = np.hstack([ abs_d_data > 1.5 * np.pi , [False]])
+        mask = np.hstack([abs_d_data > 1.5 * np.pi, [False]])
         masked_data = np.ma.MaskedArray(x_data, mask)
         self.line.set_xdata(masked_data)
-
 
         return (self.line, )
 
 
-
-
-
 class MarkedupImage(object):
-    '''Class to encapsulate drawing the image the shape + (maybe) fringes on it'''
-    def __init__(self, ax, limits, draw_fringes=False, draw_curve=True, clims=(.5, 1.5)):
+    '''Class to encapsulate drawing the image
+    the shape + (maybe) fringes on it'''
+    def __init__(self, ax, limits, draw_fringes=False,
+                 draw_curve=True, clims=(.5, 1.5)):
         '''
         :param ax: `matplotlib.Axes` object to draw into
         :param limits: [x_min, x_max, y_min, y_max]
@@ -252,13 +246,13 @@ class MarkedupImage(object):
 
         '''
         self.ax = ax
-        ax.cla()               #just to be safe
+        ax.cla()               # just to be safe
         # # set up for the image + fringe lines
         self.img = self.ax.imshow(np.zeros((limits[1] - limits[0],
-                                            limits[3] - limits[2])),
-                                            extent=limits, cmap='gray_r',
-                                            origin='lower',
-                                            interpolation='nearest')
+                                           limits[3] - limits[2])),
+                                  extent=limits, cmap='gray_r',
+                                  origin='lower',
+                                  interpolation='nearest')
         self.img.set_clim(clims)
         self.ax.set_xlim(limits[:2])
         self.ax.set_ylim(limits[2:])
@@ -275,10 +269,10 @@ class MarkedupImage(object):
         self.center_line = None
         self.seed_curve = None
         if self.draw_curve:
-            self.center_line, = ax.plot([],[],'r')
-            self.seed_curve, = ax.plot([],[],'g')
+            self.center_line, = ax.plot([], [], 'r')
+            self.seed_curve, = ax.plot([], [], 'g')
 
-    def update(self,backend,j):
+    def update(self, backend, j):
         '''Updates the axes with the image'''
 
         # deal with image
@@ -294,35 +288,33 @@ class MarkedupImage(object):
         self.fringe_lines = []
         if self.draw_finges:
             self.fringe_lines.extend(
-                mbe.ax_plot_tracks(self.img_ax,
+                mbe.ax_plot_tracks(self.ax,
                                    min_len=0,
-                                   all_tracks=False)
-                                   )
+                                   all_tracks=False))
 
         center_line_lst = []
 
         q = np.hstack([mbe.res[0][2], mbe.res[1][2]])
         phi = np.hstack([mbe.res[0][1], mbe.res[1][1]])
-        q,phi = mbe.res[0][2], mbe.res[0][1]
+        q, phi = mbe.res[0][2], mbe.res[0][1]
         new_curve = infra.SplineCurve.from_pts(np.vstack(mbe.curve.q_phi_to_xy(q, phi)),
                                                **mbe.params)
         new_curve.fft_filter(mbe.params['fft_filter'])
 
         if self.draw_curve:
-            x,y = new_curve.q_phi_to_xy(0,np.linspace(0, 2 * np.pi, 1000))
+            x, y = new_curve.q_phi_to_xy(0, np.linspace(0, 2 * np.pi, 1000))
             if self.center_line:
                 self.center_line.set_xdata(x + .5)
                 self.center_line.set_ydata(y + .5)
             else:
-                self.center_line = ax.plot(x,y)
+                self.center_line = self.ax.plot(x, y)
             center_line_lst.append(self.center_line)
 
-            x,y = mbe.curve.q_phi_to_xy(0,np.linspace(0, 2 * np.pi, 1000))
+            x, y = mbe.curve.q_phi_to_xy(0, np.linspace(0, 2 * np.pi, 1000))
             if self.seed_curve:
                 self.seed_curve.set_xdata(x + .5)
                 self.seed_curve.set_ydata(y + .5)
             center_line_lst.append(self.seed_curve)
-
 
         return tuple(self.fringe_lines) + (self.img, ) + tuple(center_line_lst)
 
@@ -364,14 +356,14 @@ def animate_profile(data_iter):
         miss_max = ln_helper(max_t[:2], lines[1], yscale=.543 / 2, xscale=circ)
 
         miss_bth = ln_helper([tuple(t) + tuple(tt) for
-                                t, tt in zip(max_t[:2], min_t[:2])],
-                              lines[2],
-                              yscale=.543 / 4,
-                              xscale=circ)
+                              t, tt in zip(max_t[:2], min_t[:2])],
+                             lines[2],
+                             yscale=.543 / 4,
+                             xscale=circ)
 
         miss_txt.set_text("min miss: %(i)d max miss: %(a)d" % {'i': miss_min,
                                                                'a': miss_max})
-        return (txt, miss_txt) + lines
+        return (txt, miss_txt, miss_bth) + lines
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
