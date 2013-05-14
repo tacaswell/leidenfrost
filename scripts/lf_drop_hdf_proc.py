db0024d9a05f05404974b619ef7e25a1a445746d
Thomas A Caswell <tcaswell@uchicago.edu>
2013-05-13 21:10:47 -0500

    pep8 + import fixes

--- scripts/lf_drop_hdf_proc.py
+++ scripts/lf_drop_hdf_proc.py
@@ -1,6 +1,7 @@
 import argparse
-import os
+
 import leidenfrost.infra as li
+from leidenfrost import FilePath
 import h5py
 
 import find_peaks.peakdetect as pd
@@ -8,10 +9,10 @@ import find_peaks.peakdetect as pd
 pd.init_procs(8)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("cine_base_path",help="The base path for where the (cine) data files are located")
-parser.add_argument("hdf_path",help="path, relative to base path of hdf file")
-parser.add_argument("hdf_fname",help="name of the hdf file")
-parser.add_argument("--hdf_base_path",help="The base path for where the hdf data file are located.  If not given, assumed to be the same as cine_base_path")
+parser.add_argument("cine_base_path", help="The base path for where the (cine) data files are located")
+parser.add_argument("hdf_path", help="path, relative to base path of hdf file")
+parser.add_argument("hdf_fname", help="name of the hdf file")
+parser.add_argument("--hdf_base_path", help="The base path for where the hdf data file are located.  If not given, assumed to be the same as cine_base_path")
 
 args = parser.parse_args()
 
@@ -21,15 +22,15 @@ if args.hdf_base_path:
 else:
     hdf_base_path = cine_base_path
 
-hdf_fname = li.FilePath(hdf_base_path,args.hdf_path,args.hdf_fname)
-print '/'.join(hdf_fname)
+hdf_fname = FilePath(hdf_base_path, args.hdf_path, args.hdf_fname)
+print hdf_fname.format
 
 
-stack,seed_curve = li.ProcessBackend.from_hdf_file(cine_base_path,hdf_fname)
-file_out = h5py.File('/'.join(hdf_fname),'r+')
+stack, seed_curve = li.ProcessBackend.from_hdf_file(cine_base_path, hdf_fname)
+file_out = h5py.File(hdf_fname.format, 'r+')
 
 for j in range(len(stack)):
-    mbe,seed_curve = stack.process_frame(j,seed_curve)
+    mbe, seed_curve = stack.process_frame(j, seed_curve)
     mbe.write_to_hdf(file_out)
     del mbe
     file_out.flush()
